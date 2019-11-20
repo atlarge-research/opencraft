fd56edc6bbf4445540f1f2e2472db40a6dfa62a9
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2019-11-20 08:27:19 +0100

    Add makefile
    This makefile creates a JAR and zip archive containing all dependencies.

---
+++ scripts/archive.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Create builds dir if it's not already there.
+mkdir -p builds
+
+# Create a unique filename
+DATE=$(date +"%Y%m%d")
+for i in {1..1000}; do
+	FILE=builds/${DATE}-$i
+	if [ ! -f "${FILE}.jar" ] && [ ! -f "${FILE}.zip" ]; then
+		break
+	fi
+	if [ $i -eq "1000" ]; then
+		echo "Congratulations, you've compiled Glowstone 1000 times today!"
+		echo "Unfortunately, the build script is not prepared for this and will exit now."
+		exit 1
+	fi
+done
+
+./scripts/build.sh
+cp target/glowstone.jar ${FILE}.jar
+zip -r ${FILE}.zip . --exclude builds .DS_Store
