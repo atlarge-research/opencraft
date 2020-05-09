7ffb11b375618425eae5b00038084e8696cb874f
Wessel van de Brug <w.g.vandebrug@student.tudelft.nl>
2020-05-09 12:12:05 +0200

    Add test for Channel's unsubscribe and isEmpty methods

--- src/test/java/net/glowstone/messaging/concurrent/ConcurrentChannelTest.java
+++ src/test/java/net/glowstone/messaging/concurrent/ConcurrentChannelTest.java
@@ -1,13 +1,32 @@
 package net.glowstone.messaging.concurrent;
 
-import net.glowstone.messaging.Channel;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import net.glowstone.messaging.ChannelTest;
 import net.glowstone.messaging.Subscriber;
+import org.junit.jupiter.api.Test;
 
 public class ConcurrentChannelTest extends ChannelTest {
 
     @Override
-    protected Channel<Subscriber, String> createChannel() {
+    protected ConcurrentChannel<Subscriber, String> createChannel() {
         return new ConcurrentChannel<>();
+    }
+
+    /**
+     * Verify that subscribing and unsubscribing alters the number of subscriptions.
+     */
+    @Test
+    void subscribeAndUnsubscribeTest() {
+
+        ConcurrentChannel<Subscriber, String> channel = createChannel();
+        Subscriber alice = new Subscriber("Alice");
+
+        assertTrue(channel.isEmpty());
+        channel.subscribe(alice, alice::onMessage);
+        assertFalse(channel.isEmpty());
+        channel.unsubscribe(alice);
+        assertTrue(channel.isEmpty());
     }
 }
