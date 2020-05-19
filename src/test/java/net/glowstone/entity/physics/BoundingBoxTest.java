c1178f8ac03c0ff37fa25e7b4a3fb13f0a00bbf5
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-05-19 21:27:32 +0200

    remove more star imports

--- src/test/java/net/glowstone/entity/physics/BoundingBoxTest.java
+++ src/test/java/net/glowstone/entity/physics/BoundingBoxTest.java
@@ -2,12 +2,13 @@ package net.glowstone.entity.physics;
 
 import net.glowstone.util.Vectors;
 import org.bukkit.util.Vector;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static net.glowstone.entity.physics.BoundingBox.intersects;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BoundingBoxTest {
 
@@ -18,20 +19,20 @@ public class BoundingBoxTest {
 
     @Before
     public void setup() {
-        box = BoundingBox.fromCorners(new Vector(0,0,0), new Vector(1,1,1));
-        box1 = BoundingBox.fromCorners(new Vector(-1,-1,-1), new Vector(0.1,0.1,0.1));
-        box2 = BoundingBox.fromCorners(new Vector(-2,-2,-2), new Vector(-1,-1.5,-2));
-        box3 = BoundingBox.fromCorners(new Vector(-1.5,-1.9,-2.5), new Vector(-1,-1.75,-4));
+        box = BoundingBox.fromCorners(new Vector(0, 0, 0), new Vector(1, 1, 1));
+        box1 = BoundingBox.fromCorners(new Vector(-1, -1, -1), new Vector(0.1, 0.1, 0.1));
+        box2 = BoundingBox.fromCorners(new Vector(-2, -2, -2), new Vector(-1, -1.5, -2));
+        box3 = BoundingBox.fromCorners(new Vector(-1.5, -1.9, -2.5), new Vector(-1, -1.75, -4));
     }
 
     @Test
-    public void intersectTest(){
+    public void intersectTest() {
         assertTrue(box.intersects(box1));
-        assertTrue(intersects(box1,box, Double.MIN_VALUE));
+        assertTrue(intersects(box1, box, Double.MIN_VALUE));
     }
 
     @Test
-    public void intersectTestWithoutIntersect(){
+    public void intersectTestWithoutIntersect() {
         assertFalse(box2.intersects(box));
         assertFalse(intersects(box, box2, Double.MIN_VALUE));
         assertFalse(box2.intersects(box1));
@@ -41,9 +42,9 @@ public class BoundingBoxTest {
     }
 
     @Test
-    public void positionAndSizeTest(){
-        Vector origin = new Vector(0,0,0);
-        Vector size = new Vector(1,1,1);
+    public void positionAndSizeTest() {
+        Vector origin = new Vector(0, 0, 0);
+        Vector size = new Vector(1, 1, 1);
 
         BoundingBox otherBox = BoundingBox.fromPositionAndSize(origin, size);
         assertTrue(Vectors.equals(otherBox.minCorner, origin));
@@ -51,12 +52,12 @@ public class BoundingBoxTest {
     }
 
     @Test
-    public void copyOfTest(){
+    public void copyOfTest() {
         assertNotEquals(box, BoundingBox.copyOf(box));
     }
 
     @Test
     public void getSizeTest() {
-        assertTrue(Vectors.equals(box.getSize(), new Vector(1,1,1)));
+        assertTrue(Vectors.equals(box.getSize(), new Vector(1, 1, 1)));
     }
 }
