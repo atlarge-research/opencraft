ece46f94221c586ec33dbae3c90c5897cc080f9a
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-05-19 21:33:44 +0200

    add spaces

--- src/test/java/net/glowstone/util/VectorsTest.java
+++ src/test/java/net/glowstone/util/VectorsTest.java
@@ -11,7 +11,7 @@ public class VectorsTest {
     private double EPSILON = 0.0000000000001d;
 
     @Test
-    public void floorTest(){
+    public void floorTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector floored = Vectors.floor(vector);
         assertEquals(floored.getX(), 0.0, EPSILON);
@@ -20,7 +20,7 @@ public class VectorsTest {
     }
 
     @Test
-    public void ceilTest(){
+    public void ceilTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector ceiled = Vectors.ceil(vector);
         assertEquals(ceiled.getX(), 1.0, EPSILON);
@@ -29,7 +29,7 @@ public class VectorsTest {
     }
 
     @Test
-    public void projectTest(){
+    public void projectTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector normal = new Vector(1.0, 0.0, 0.0);
         Vector projected = Vectors.project(vector, normal);
@@ -40,7 +40,7 @@ public class VectorsTest {
     }
 
     @Test
-    public void projectTest1(){
+    public void projectTest1() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector normal = new Vector(0.0, 1.0, 0.0);
         Vector projected = Vectors.project(vector, normal);
@@ -51,7 +51,7 @@ public class VectorsTest {
     }
 
     @Test
-    public void projectTest2(){
+    public void projectTest2() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector normal = new Vector(0.0, 0.0, 1.0);
         Vector projected = Vectors.project(vector, normal);
@@ -62,27 +62,27 @@ public class VectorsTest {
     }
 
     @Test
-    public void equalsTest(){
+    public void equalsTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
-        Vector otherVector = new Vector(0.2/2.0, 2.0/2.0, -1.8/2.0);
+        Vector otherVector = new Vector(0.2 / 2.0, 2.0 / 2.0, -1.8 / 2.0);
 
         assertEquals(Vectors.equals(vector, otherVector, EPSILON), true);
     }
 
     @Test
-    public void equalEarlyExitXTest(){
+    public void equalEarlyExitXTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector otherVector = mock(Vector.class);
 
         when(otherVector.getX()).thenReturn(0.0);
         assertEquals(Vectors.equals(vector, otherVector, EPSILON), false);
 
-        verify(otherVector,times(0)).getY();
+        verify(otherVector, times(0)).getY();
 
     }
 
     @Test
-    public void equalEarlyExitYTest(){
+    public void equalEarlyExitYTest() {
         Vector vector = new Vector(0.1, 1.0, -0.9);
         Vector otherVector = mock(Vector.class);
 
@@ -90,7 +90,7 @@ public class VectorsTest {
         when(otherVector.getX()).thenReturn(0.5);
         assertEquals(Vectors.equals(vector, otherVector, EPSILON), false);
 
-        verify(otherVector,times(0)).getZ();
+        verify(otherVector, times(0)).getZ();
 
     }
 }
