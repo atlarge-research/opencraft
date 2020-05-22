0d8a2d148f6aae40092fe965bccf02356355e18c
wubero <r.j.marang@student.tudelft.nl>
2020-05-22 10:27:03 +0200

    Fix unsubscribe method

--- src/main/java/net/glowstone/messaging/brokers/guava/GuavaBroker.java
+++ src/main/java/net/glowstone/messaging/brokers/guava/GuavaBroker.java
@@ -5,37 +5,47 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import net.glowstone.messaging.Broker;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class GuavaBroker<Topic, Subscriber, Message> implements Broker<Topic, Subscriber, Message> {
 
     private final Map<Topic, EventBus> channels;
+    private final Map<Pair<Topic, Subscriber>, GuavaListener> subscribers;
 
     public GuavaBroker() {
         channels = new ConcurrentHashMap<>();
+        subscribers = new ConcurrentHashMap<>();
     }
 
     @Override
     public void subscribe(Topic topic, Subscriber subscriber, Consumer<Message> callback) {
+
         GuavaListener<Subscriber, Message> listener = new GuavaListener<>(subscriber, callback);
+        Pair<Topic, Subscriber> pair = Pair.of(topic, subscriber);
+        subscribers.put(pair, listener);
+
         EventBus channel = channels.computeIfAbsent(topic, t -> new EventBus());
         channel.register(listener);
     }
 
     @Override
     public void unsubscribe(Topic topic, Subscriber subscriber) {
-        GuavaListener<Subscriber, Message> listener = new GuavaListener<>(subscriber, null);
-        EventBus channel = channels.getOrDefault(topic, new EventBus());
-        try {
-            channel.unregister(listener);
+        Pair<Topic, Subscriber> pair = Pair.of(topic, subscriber);
+        GuavaListener<Subscriber, Message> listener = subscribers.getOrDefault(pair, null);
+        EventBus channel = channels.computeIfAbsent(topic, t -> new EventBus());
+        if (listener != null) {
+            try {
+                channel.unregister(listener);
 
-        } catch (IllegalArgumentException e) {
-            //
+            } catch (IllegalArgumentException ignored) {
+                // Don't need to do anything when a listener is not used.
+            }
         }
     }
 
     @Override
     public void publish(Topic topic, Message message) {
-        EventBus channel = channels.getOrDefault(topic, new EventBus()); // Look if thread safe
+        EventBus channel = channels.getOrDefault(topic, new EventBus()); //
         channel.post(message);
     }
 }
