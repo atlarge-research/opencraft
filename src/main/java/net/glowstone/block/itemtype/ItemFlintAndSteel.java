420c29431dac1b9071977b41fd2f845574e05e7a
Lars de Tombe <l.j.detombe@student.tudelft.nl>
2020-05-25 11:56:54 +0200

    Improve nether portal creation

--- src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
+++ src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
@@ -1,10 +1,12 @@
 package net.glowstone.block.itemtype;
 
 import net.glowstone.EventFactory;
+import net.glowstone.GlowWorld;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.ItemTable;
 import net.glowstone.block.blocktype.BlockTnt;
 import net.glowstone.entity.GlowPlayer;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockIgniteEvent;
@@ -37,16 +39,87 @@ public class ItemFlintAndSteel extends ItemTool {
         return false;
     }
 
+    /**
+     * Retrieve the horizontal direction the portal should be made in. This method assumes a portal is 2 wide. We
+     * check on which side of the block an obsidian block is, the opposite direction is the direction the portal
+     * should be made in.
+     * @param block The block in a portal
+     * @return The direction of the portal.
+     */
+    private BlockFace getHorizontalPortalDirection(GlowBlock block) {
+        if (block.getRelative(BlockFace.NORTH).getType() == Material.OBSIDIAN) {
+            return BlockFace.SOUTH;
+        } else if (block.getRelative(BlockFace.EAST).getType() == Material.OBSIDIAN) {
+            return BlockFace.WEST;
+        } else if (block.getRelative(BlockFace.SOUTH).getType() == Material.OBSIDIAN) {
+            return BlockFace.NORTH;
+        } else if (block.getRelative(BlockFace.WEST).getType() == Material.OBSIDIAN) {
+            return BlockFace.EAST;
+        }
+        return null;
+    }
+
+    /**
+     * Checks if there is a 2x3 portal.
+     * @param block A block in a corner of the portal
+     * @param verticalFace Whether we have to build the portal up or down.
+     * @return If this is a valid portal.
+     */
+    private boolean isPortal(GlowBlock block, BlockFace verticalFace) {
+        BlockFace direction = getHorizontalPortalDirection(block);
+        if (direction == null) {
+            return false;
+        }
+
+        BlockFace oppositeFace = verticalFace.getOppositeFace();
+        GlowBlock oppositeBlock = block.getRelative(oppositeFace);
+        if (oppositeBlock.getType() != Material.OBSIDIAN || oppositeBlock.getRelative(direction).getType() != Material.OBSIDIAN) {
+            return false;
+        }
+
+        for (int i = 1; i <=3; i++) {
+
+            if(block.getRelative(direction, 2).getType() != Material.OBSIDIAN){
+                return false;
+            }
+
+            BlockFace oppositeDirection = direction.getOppositeFace();
+            if(block.getRelative(oppositeDirection).getType() != Material.OBSIDIAN){
+                return false;
+            }
+
+            block = block.getRelative(verticalFace);
+        }
+
+        if (block.getType() != Material.OBSIDIAN || block.getRelative(direction).getType() != Material.OBSIDIAN) {
+            return false;
+        }
+
+        return true;
+    }
+
     private void fireNetherPortal(GlowBlock target, BlockFace face) {
         if (face == BlockFace.UP || face == BlockFace.DOWN) {
+
             target = target.getRelative(face);
+            if (!isPortal(target, face)){
+                return;
+            }
+
+            BlockFace direction = getHorizontalPortalDirection(target);
+            GlowBlock sideBlock = target.getRelative(direction);
+
+            GlowWorld world = target.getWorld();
             int limit = 0;
-            // TODO: Make sure the whole nether portal is filled with portals
-            while ((target.getType() == Material.AIR) && limit < 23) {
+
+            while ((target.getType() == Material.AIR) && limit < 4) {
                 target.setType(Material.PORTAL);
+                sideBlock.setType(Material.PORTAL);
                 target = target.getRelative(face);
+                sideBlock = sideBlock.getRelative(face);
                 limit++;
             }
+
         }
     }
 
