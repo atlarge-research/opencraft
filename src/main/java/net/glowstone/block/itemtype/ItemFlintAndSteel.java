ce7929506d5246bbbc919c1640464ef9941eb005
Lars de Tombe <l.j.detombe@student.tudelft.nl>
2020-05-28 12:19:52 +0200

    Add start for dynamic portal sizes

--- src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
+++ src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
@@ -7,7 +7,10 @@ import net.glowstone.block.GlowBlock;
 import net.glowstone.block.ItemTable;
 import net.glowstone.block.blocktype.BlockTnt;
 import net.glowstone.entity.GlowPlayer;
+import org.apache.commons.lang3.tuple.Pair;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
@@ -16,6 +19,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class ItemFlintAndSteel extends ItemTool {
+
+    private static final int PORTAL_SIZE = 20;
 
     public ItemFlintAndSteel() {
         this.setPlaceAs(Material.FIRE);
@@ -61,6 +66,96 @@ public class ItemFlintAndSteel extends ItemTool {
         }
 
         return null;
+    }
+
+    private boolean isValidPortal(GlowBlock block) {
+
+        Pair<GlowBlock, BlockFace> portalPos = getPortalPosition(block);
+        if (portalPos == null) {
+            return false;
+        }
+
+        GlowBlock cornerBlock = portalPos.getLeft();
+        BlockFace direction = portalPos.getRight();
+
+        int height = getPortalLengthDirection(cornerBlock, BlockFace.UP);
+        int width = getPortalLengthDirection(cornerBlock, direction);
+        if (height == -1 || height < 3 || width == -1 || width < 2) {
+            return false;
+        }
+
+        GlowBlock blockFloor = cornerBlock.getRelative(BlockFace.DOWN);
+        for (int i = 0; i < width; i++){
+            if (blockFloor.getType() != Material.OBSIDIAN) {
+                return false;
+            }
+            blockFloor = blockFloor.getRelative(direction);
+        }
+
+        GlowBlock sideBlock = cornerBlock.getRelative(direction.getOppositeFace());
+        GlowBlock oppositeSideBlock = cornerBlock.getRelative(direction, width + 1);
+        for (int i = 0; i < height; i++){
+            if (sideBlock.getType() != Material.OBSIDIAN) {
+                return false;
+            } else if (oppositeSideBlock.getType() != Material.OBSIDIAN) {
+                return false;
+            }
+            sideBlock = sideBlock.getRelative(BlockFace.UP);
+            oppositeSideBlock = oppositeSideBlock.getRelative(BlockFace.UP);
+        }
+
+        //GlowBlock blockCeiling = cornerBlock.getRelative()
+
+        return false;
+    }
+
+    /**
+     * Get the air block in the corner of the portal.
+     *
+     * @param block The block to start checking for the position.
+     * @return The position and direction of the corner of a portal. This position will always be at the bottom.
+     */
+    private Pair<GlowBlock, BlockFace> getPortalPosition(GlowBlock block) {
+        int heightDown = getPortalLengthDirection(block, BlockFace.DOWN);
+        int widthSouth = getPortalLengthDirection(block, BlockFace.SOUTH);
+        int widthWest = getPortalLengthDirection(block, BlockFace.WEST);
+
+        if (heightDown != -1 && widthSouth != -1) {
+            GlowBlock blockPosition = block.getRelative(BlockFace.DOWN, heightDown);
+            blockPosition = blockPosition.getRelative(BlockFace.SOUTH, widthSouth);
+            return Pair.of(blockPosition, BlockFace.NORTH);
+        } else if (heightDown != -1 && widthWest != -1) {
+            GlowBlock blockPosition = block.getRelative(BlockFace.DOWN, heightDown);
+            blockPosition = blockPosition.getRelative(BlockFace.WEST, widthSouth);
+            return Pair.of(blockPosition, BlockFace.EAST);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the length from the current block to the first obsidian block. If the first block it encounters is not
+     * obsidian or the length exceeds the portal size it is invalid. The given block should not be an obsidian block,
+     * but always an air block.
+     *
+     * @param block The block to start the measurement from.
+     * @param direction The direction to check for the length.
+     * @return The length to the first obsidian block in the given direction.
+     */
+    private int getPortalLengthDirection(GlowBlock block, BlockFace direction) {
+        int size = -1;
+
+        while (size < PORTAL_SIZE  && block.getType() == Material.AIR) {
+            size++;
+            block = block.getRelative(direction);
+
+            if (block.getType() != Material.AIR || block.getType() != Material.OBSIDIAN) {
+                size = -1;
+                break;
+            }
+        }
+
+        return size;
     }
 
     /**
