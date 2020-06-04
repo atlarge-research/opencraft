14072e688aafd6773c0c38da1853eab8f7956758
Wessel van de Brug <w.g.vandebrug@student.tudelft.nl>
2020-06-04 15:36:51 +0200

    Add missing whiteline before class definition

--- src/main/java/net/glowstone/util/config/ChannelConfig.java
+++ src/main/java/net/glowstone/util/config/ChannelConfig.java
@@ -1,6 +1,7 @@
 package net.glowstone.util.config;
 
 import lombok.Data;
+
 /**
  * The channel config provides a collection of configuration options that can be parsed together for use in the broker
  * factory, in particular in the newChannelFactory method.
