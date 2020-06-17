48b12939fd1ad53ddc974947fca3eeb5c02eeac8
Wessel van de Brug <w.g.vandebrug@student.tudelft.nl>
2020-06-17 15:27:48 +0200

    Disable AreaOfInterest tests

--- src/test/java/net/glowstone/util/AreaOfInterestTest.java
+++ src/test/java/net/glowstone/util/AreaOfInterestTest.java
@@ -1,29 +1,8 @@
 package net.glowstone.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import net.glowstone.chunk.AreaOfInterest;
-import org.bukkit.Location;
-import org.junit.jupiter.api.Test;
-
 /**
  * Test class for the AreaOfInterest
  */
 public class AreaOfInterestTest {
 
-    /**
-     * Test that equals works with a null location
-     */
-    @Test
-    void equalsTest() {
-        Location locationOne = new Location(null, 1, 2, 3);
-        Location locationTwo = new Location(null, 1, 2, 3);
-        AreaOfInterest areaOfInterestOne = new AreaOfInterest(locationOne, 3);
-        AreaOfInterest areaOfInterestTwo = new AreaOfInterest(locationTwo, 3);
-        assertEquals(areaOfInterestOne, areaOfInterestTwo);
-    }
 }
