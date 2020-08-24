9e3619506460642084a37a69b49819a910ee6c22
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2020-08-24 15:41:33 +0200

    Changeed PopulateInfo to account for both sending and receiving.

--- src/main/java/net/glowstone/lambda/population/serialization/PopulateInfo.java
+++ src/main/java/net/glowstone/lambda/population/serialization/PopulateInfo.java
@@ -1,26 +1,53 @@
 package net.glowstone.lambda.population.serialization;
 
 import com.google.gson.Gson;
+import java.util.List;
+import java.util.Random;
 import net.glowstone.GlowWorld;
+import net.glowstone.chunk.GlowChunk;
+import net.glowstone.net.message.play.game.BlockChangeMessage;
+
 
 public class PopulateInfo {
     private static final Gson gson = JsonUtil.getGson();
 
-    public GlowWorld world;
-    public int x;
-    public int z;
+    public static class PopulateInput {
+        public GlowWorld world;
+        public Random random;
+        public int x;
+        public int z;
 
-    public PopulateInfo(GlowWorld world, int x, int z) {
-        this.world = world;
-        this.x = x;
-        this.z = z;
+        public PopulateInput(GlowWorld world, Random random, int x, int z) {
+            this.world = world;
+            this.random = random;
+            this.x = x;
+            this.z = z;
+        }
+
+        public String toJson() {
+            return gson.toJson(this);
+        }
+
+        public static PopulateInput fromJson(String jsn) {
+            return gson.fromJson(jsn, PopulateInput.class);
+        }
     }
 
-    public String toJson() {
-        return gson.toJson(this);
-    }
+    public static class PopulateOutput {
+        List<BlockChangeMessage> changedBlocks;
+        List<GlowChunk> populatedChunks;
 
-    public static PopulateInfo fromJson(String jsn) {
-        return gson.fromJson(jsn, PopulateInfo.class);
+        public PopulateOutput(List<BlockChangeMessage> changedBlocks, List<GlowChunk> populatedChunks) {
+            this.changedBlocks = changedBlocks;
+            this.populatedChunks = populatedChunks;
+        }
+
+        public String toJson() {
+            return gson.toJson(this);
+        }
+
+        public static PopulateOutput fromJson(String jsn) {
+            return gson.fromJson(jsn, PopulateOutput.class);
+        }
     }
 }
