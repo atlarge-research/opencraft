d3e373636a564e8e7073291ee58cb902d5c8672d
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2020-08-31 19:17:10 +0200

    Fixed a bug where some tree types would never spawn

--- src/main/java/net/glowstone/lambda/population/serialization/adapters/TreeDecorationSerializer.java
+++ src/main/java/net/glowstone/lambda/population/serialization/adapters/TreeDecorationSerializer.java
@@ -36,17 +36,27 @@ public class TreeDecorationSerializer implements JsonSerializer<TreeDecoration> 
 
         GenericTree tree = src.getConstructor().apply(new Random(), new BlockStateDelegate());
 
-        Class<?> toAdd = null;
+        if (tree == null) {
+            return null;
+        }
+
+        Class<?> toAdd;
         if (tree instanceof AcaciaTree) {
             toAdd = AcaciaTree.class;
-        } else if (tree instanceof BigOakTree) {
-            toAdd = BigOakTree.class;
         } else if (tree instanceof BigTree) {
             toAdd = BigTree.class;
+        } else if (tree instanceof BigOakTree) {
+            toAdd = BigOakTree.class;
+        } else if (tree instanceof TallBirchTree) {
+            toAdd = TallBirchTree.class;
         } else if (tree instanceof BirchTree) {
             toAdd = BirchTree.class;
+        } else if (tree instanceof RedMushroomTree) {
+            toAdd = RedMushroomTree.class;
         } else if (tree instanceof BrownMushroomTree) {
             toAdd = BrownMushroomTree.class;
+        }  else if (tree instanceof SwampTree) {
+            toAdd = SwampTree.class;
         } else if (tree instanceof CocoaTree) {
             toAdd = CocoaTree.class;
         } else if (tree instanceof DarkOakTree) {
@@ -55,27 +65,23 @@ public class TreeDecorationSerializer implements JsonSerializer<TreeDecoration> 
             toAdd = JungleBush.class;
         } else if (tree instanceof JungleTree) {
             toAdd = JungleTree.class;
-        } else if (tree instanceof MegaPineTree) {
-            toAdd = MegaPineTree.class;
-        } else if (tree instanceof MegaJungleTree) {
-            toAdd = MegaJungleTree.class;
         } else if (tree instanceof MegaSpruceTree) {
             toAdd = MegaSpruceTree.class;
+        } else if (tree instanceof MegaPineTree) {
+            toAdd = MegaPineTree.class;
         } else if (tree instanceof MegaRedwoodTree) {
             toAdd = MegaRedwoodTree.class;
-        } else if (tree instanceof RedwoodTree) {
-            toAdd = RedwoodTree.class;
-        } else if (tree instanceof RedMushroomTree) {
-            toAdd = RedMushroomTree.class;
-        } else if (tree instanceof SwampTree) {
-            toAdd = SwampTree.class;
-        } else if (tree instanceof TallBirchTree) {
-            toAdd = TallBirchTree.class;
+        } else if (tree instanceof MegaJungleTree) {
+            toAdd = MegaJungleTree.class;
         } else if (tree instanceof TallRedwoodTree) {
             toAdd = TallRedwoodTree.class;
+        } else if (tree instanceof RedwoodTree) {
+            toAdd = RedwoodTree.class;
+        } else {
+            toAdd = GenericTree.class;
         }
 
-        obj.addProperty("constructor", toAdd != null ? toAdd.getCanonicalName() : "");
+        obj.addProperty("constructor", toAdd.getCanonicalName());
         return obj;
     }
 }
