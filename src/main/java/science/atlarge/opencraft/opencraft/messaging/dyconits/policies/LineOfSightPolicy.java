2c986991c636cc5e0a919fc89fdb4e86bb993221
Razvan Wist <razvan.wist@gmail.com>
2023-06-26 11:01:44 +0200

    go back

--- src/main/java/science/atlarge/opencraft/opencraft/messaging/dyconits/policies/LineOfSightPolicy.java
+++ src/main/java/science/atlarge/opencraft/opencraft/messaging/dyconits/policies/LineOfSightPolicy.java
@@ -1,9 +1,6 @@
 package science.atlarge.opencraft.opencraft.messaging.dyconits.policies;
 
 import com.flowpowered.network.Message;
-
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType.Object;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -98,7 +95,7 @@ public class LineOfSightPolicy implements DyconitPolicy<Player, Message> {
             chunksVisibleSet.add(visibleBlock.getChunk());
         }
 
-        chunks.add(new DyconitSubscribeCommand<>(sub.getKey(), sub.getCallback(), Bounds.Companion.getZERO(), CATCH_ALL_DYCONIT_NAME));
+        chunks.add(new DyconitSubscribeCommand<>(sub.getKey(), sub.getCallback(), Bounds.Companion.getINFINITE(), CATCH_ALL_DYCONIT_NAME));
 
         for (Chunk visibleChunk : chunksVisibleSet) {
             String dyconitName = chunkToName(visibleChunk);
@@ -106,7 +103,7 @@ public class LineOfSightPolicy implements DyconitPolicy<Player, Message> {
             int z = visibleChunk.getZ();
             double d = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(z - centerZ, 2));
 
-            chunks.add(new DyconitSubscribeCommand<>(sub.getKey(), sub.getCallback(), new Bounds(1000, 2), dyconitName));
+            chunks.add(new DyconitSubscribeCommand<>(sub.getKey(), sub.getCallback(), new Bounds((int)Math.round(d) * (int)Math.round(d) * 50, -1), dyconitName));
             playerSubscriptions.add(dyconitName);
         }
 
